f3236509713571dbdf098909822688051fb7b7f9
AliceRaker <AliceRakers.NEO@gmail.com>
2013-02-01 14:19:37 +0900

    CheckKeyWord()を作成.1つでもkeywordsにかかるものがあったらSendMention()に処理を投げる。

--- src/CheckKeyword.rb
+++ src/CheckKeyword.rb
@@ -18,8 +18,36 @@
 # すべて調べていたら処理が重くなるのと、SendMentions.rbの方と処理がかぶるので
 
 def CheckKeyword(old_time, new_time)
-    # old_time以上、new_time未満でお願いします
-    return true
+    # old_time以上、new_time未満のtimelineにkeywordにかかるものがあるかどうか判定
+
+    keywords=[]
+    data=[]
+    Dir::glob("../data/keyword/*.csv").each{|filename|
+    	File.read(filename).split("\n").each do |data|
+    		keywords.unshift(data)
+		end
+}
+
+    Twitter.home_timeline.each do |tweet|
+        # 前回実行時から今回実行時までの間
+        if old_time < tweet.created_at && tweet.created_at <= new_time
+            # 自分以外のツイートとリプライを除いたもののみ
+            if tweet.user.screen_name != USER_NAME &&
+               !tweet.text.include?("@"+USER_NAME) then
+            	keywords.each do |pattern|
+            		#print(pattern) #debag
+     		   		if (tweet.text).include?(pattern)
+    					return true
+    					break
+            		end
+            	end
+            end
+        # 時間外ならループを抜ける
+        else
+            break
+        end
+    end
+
 end
 
 # デバッグ用
