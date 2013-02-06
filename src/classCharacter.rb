d8e430d7ceeba664868beafff0338a6c268db082
AliceRaker <AliceRakers.NEO@gmail.com>
2013-02-06 13:42:27 +0900

    MySQLを利用する場合のlayerとcheck_keywordの処理をスケルトンコードで追加

--- src/classCharacter.rb
+++ src/classCharacter.rb
@@ -39,6 +39,10 @@ class Character
                 # なにもなかった場合は飛ばす
                 next
             end
+
+            #layer処理をここに挿入
+            #layer(tweet.text)
+
             # 選択したResponderからresとoptionsを受け取る
             res = @responder.response(tweet, @emotion.mood)
             options = @responder.set_options(tweet)
@@ -63,6 +67,33 @@ class Character
         end
         return resp
     end
+
+=begin
+    #MySQLの宣言
+    mysql = Mysql.new()
+
+    #layer処理
+    def layer(tweet)
+    if(type = check_keyword(tweet,1)){
+        #挨拶処理
+        #type に oha とか oya とか種類を返してくるので、それにあったリプライ内容を取得する@ResponderサブクラスかReplyResponderの拡張が必要
+    }
+    if(type = check_keyword(tweet,0)){
+        #好感度処理
+        #高感度の仕様が固まってないから何とも typeに文字列で-10とか返させて、それを数値に変換した後そのまま高感度の加減処理すればいいかなあ
+    }
+    end
+
+    #キーワードチェック
+    def check_keyword(tweet,layer)
+        #tweetにphraseが含まれる要素があればひとつ返す(現在はDB登録順で一番早いもの)
+        check = mysql.query("SELECT * FROM keywords where LOCATE(phrase,".tweet.") > 0 and layer = ".layer." limit 1")
+        
+        #checkがnullでないとき、typeの値を返す
+        if(!check){return check[1]}
+    end
+
+=end
 
     # !現在は使っていない
     # 学習した内容を辞書に追加
