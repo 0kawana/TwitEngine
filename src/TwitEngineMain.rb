40637c289fbe4cd2f0a24523c0bf4ee5303c8e45
gembaf <gembaf.github@gmail.com>
2013-04-02 04:17:31 +0900

    change add user

--- src/TwitEngineMain.rb
+++ src/TwitEngineMain.rb
@@ -69,8 +69,9 @@ class Mary
         # フォロー・フォロワーの整理
         # if @new_time.hour == 4 and (@new_time.min == 0 or @new_time.min == 1 or @new_time.min == 3)
         #if @new_time.hour == 4 and @new_time.min == 0 # 一番人がいなさそうな午前４時で
+        if @new_time.min == 0
             @mary.adjust_user
-        #end
+        end
 
         html = "Action successful!!<BR>
                 #{@old_time.mon}/#{@old_time.day},#{@old_time.hour}:#{@old_time.min}:#{@old_time.sec} ~ #{@new_time.mon}/#{@new_time.day},#{@new_time.hour}:#{@new_time.min}:#{@new_time.sec}"
