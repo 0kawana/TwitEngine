c0aa812c4ebddc7d392bab12a34f210d3bfdaf15
AliceRaker <AliceRakers.NEO@gmail.com>
2013-05-23 01:31:11 +0900

    不要なファイルを削除

--- cron/post.py
+++
@@ -1,8 +0,0 @@
-from google.appengine.api import urlfetch
-url = "http://twit-engine-mary.herokuapp.com/"
-result = urlfetch.fetch(url=url,
-                        method=urlfetch.GET,
-                        headers={'Cache-Control':'max-age=0'},
-                        deadline=10)
-if result.status_code == 200:
-    print result.content
