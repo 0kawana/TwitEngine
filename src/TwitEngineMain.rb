1b81ff52e003c2aedbbca9d4a47911fc01f5030d
gembaf <gembaf.github@gmail.com>
2013-06-20 17:40:26 +0900

    一応完成

--- src/TwitEngineMain.rb
+++ src/TwitEngineMain.rb
@@ -12,30 +12,65 @@
 require 'rubygems'
 require 'twitter'
 require File.expand_path(File.dirname(__FILE__) + '/define.rb')
+require File.expand_path(File.dirname(__FILE__) + '/classTwitterControl.rb')
 require File.expand_path(File.dirname(__FILE__) + '/classCharacter.rb')
 
 class Mary
-    # initialize
     def initialize
+        @twit_ctrl = TwitterControl.new
+        @old_time = get_oldtime()
+        @new_time = get_newtime()
+        @character = Character.new(@twit_ctrl, @new_time)
     end
 
-    # tweet
+    # @return [String]
     def tweet
+        write_newtime()
+        timeline = []
+        timeline += @twit_ctrl.get_mentions(@old_time, @new_time, ajust_time(0.0))
+        timeline += @twit_ctrl.get_timeline(@old_time, @new_time, ajust_time(0.0))
+
+        posts = @character.dialogue(timeline)
+        @twit_ctrl.tweet_posts(posts)
+
+        html = "Action Successfull!!<BR>
+        #{@old_time.mon}/#{@old_time.day},#{@old_time.hour}:#{@old_time.min}:#{@old_time.sec} ~
+        #{@new_time.mon}/#{@new_time.day},#{@new_time.hour}:#{@new_time.min}:#{@new_time.sec}"
+        return html
     end
+
 
     protected
-    # get_oldtime
+    # @param time [Time or Float]
+    # @return [Time or Float]
+    def ajust_time(time)
+        #time += 9*60*60   #=> heroku(もとがGMTなので)
+        return time
+    end
+
+    # @return [Time]
     def get_oldtime
+        return get_newtime() unless File.exists?("./Runtime.log")
+
+        log = File.read("./Runtime.log").split(",")
+        t = Time.local(*log)
+        old_time = ajust_time(t.localtime)
+        return old_time
     end
 
-    # write_newtime
+    # @return [Time]
+    def get_newtime
+        time = ajust_time(Time.new)
+        return time
+    end
+
     def write_newtime
-    end
-
-    # tweet_post(Array[Hash])
-    def tweet_post(post)
+        File.open("./Runtime.log", 'w') do |f|
+            f.print @new_time.strftime("%Y,%m,%d,%H,%M,%S")
+        end
     end
 end
 
+puts Mary.new.tweet
 
 
