7b5443adc1b17556dfd5150b191672fc72ff2664
gembaf <gembaf.github@gmail.com>
2013-06-22 16:21:26 +0900

    implement Emotion class

--- src/classCharacter.rb
+++ src/classCharacter.rb
@@ -23,13 +23,13 @@ class Character
 
     # @param twit_ctrl [TwitterControl]
     # @param new_time [Time]
-    # @param ajust_time [Time or Float]
-    def initialize(twit_ctrl, new_time, ajust_time)
+    # @param adjust_time [Time or Float]
+    def initialize(twit_ctrl, new_time, adjust_time)
         @twit_ctrl = twit_ctrl
         @new_time = new_time
-        @ajust_time = ajust_time
-        @dictionary = Dictionary.new(@twit_ctrl, @new_time, @ajust_time)
-        #@dictionary = TestDictionary.new(@twit_ctrl, @new_time, @ajust_time)  #=> debug
+        @adjust_time = adjust_time
+        @dictionary = Dictionary.new(@twit_ctrl, @new_time, @adjust_time)
+        #@dictionary = TestDictionary.new(@twit_ctrl, @new_time, @adjust_time)  #=> debug
         @emotion = Emotion.new(@dictionary)
     end
 
@@ -46,6 +46,20 @@ class Character
         ]
         return posts
     end
+
+    def adjust_user
+        followers = @twit_ctrl.follower_ids(NAME)
+        friends = @twit_ctrl.friend_ids(NAME)
+        removes = friends - followers 
+        registers = followers - friends
+
+        @dictionary.remove_users(removes)
+        @dictionary.regist_users(registers)
+
+        @twit_ctrl.unfollow(removes)
+        @twit_ctrl.follow(registers)
+    end
+
 
     private
     # @param tweet [Tweet]
@@ -72,14 +86,28 @@ class Emotion
 
     # @param dict [Dictionary]
     def initialize(dict)
+        @dict = dict
     end
 
-    # @param id_str [String]
+    # @param user_key [String]
     # @param val [Integer]
-    def update_mood(id_str, val)
+    def update_mood(user_key, val)
+        mood = @dict.userdata[user_key].user_mood + val
+        mood = MOOD_MAX if mood > MOOD_MAX
+        mood = MOOD_MIN if mood < MOOD_MIN
+        @dict.update_mood(user_key, mood)
     end
 
-    def ajust_mood
+    # @note 全ユーザの機嫌値を0に近づける
+    def adjust_mood
+        @dict.userdata.each_pair do |key, value|
+            mood = value.user_mood
+            next if mood == 0
+
+            mood -= MOOD_RECOVERY if mood > 0
+            mood += MOOD_RECOVERY if mood < 0
+            @dict.update_mood(key, mood)
+        end
     end
 end
 
